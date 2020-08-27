9841f49fb91fea60f0091c49007980823a8ce612
eliezerantonio <eliezerfernandoantonio@gmail.com>
2020-08-27 02:35:54 +0100

    s

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # flutter_despesas_pessoais
-# flutter_despesas_pessoais
+
---
+++ android/app/src/main/kotlin/com/example/despesas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.despesas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
