f02c06f54d9c2b57fc41ecc60dfd9b85f3372acb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2020-08-29 05:01:04 +0100

    Crud completo

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,71 @@
+import 'package:despesas/models/transaction.dart';
+import 'package:despesas/widgets/chart_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+//verificando se o dia e mesmo para somar os valores
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        //pegando letra iniciar da semana
+        'value': totalSum
+      };
+    }).reversed.toList();
+  }
+
+  //calculca totla gasto na semana
+
+  double get _weekTotalValue {
+    //acomulador e elemento actual
+    return groupedTransactions.fold(0.0, (soma, transacao) {
+      return soma + transacao['value'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((transaction) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: transaction['day'],
+                value: transaction['value'],
+                percentage: _weekTotalValue == 0
+                    ? 0
+                    : (transaction['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/transaction_form.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double, DateTime) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime _selectDate = DateTime.now();
+
+  _submitForm() {
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0 || _selectDate == null) {
+      return;
+    }
+    widget.onSubmit(title, value, _selectDate);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              controller: _valueController,
+              decoration: InputDecoration(labelText: 'Valor (KZ)'),
+              onSubmitted: (_) => _submitForm(),
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      _selectDate == null
+                          ? "Nenhuma data Selecionada!"
+                          : 'Data selecionada: ${DateFormat('d/M/y').format(_selectDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      "Selecionar Data",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _showDatePicker,
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RaisedButton(
+                  child: Text("Nova Transação"),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button.color,
+                  onPressed: _submitForm,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
