4993c35d96d932fc39c4398e9e35542fa11d4a3b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2020-08-31 05:01:16 +0100

    a

---
+++ not/adaptative_text_field copy(copiar)(copiar)(copiar 3)(copiar).dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+class AdaptativeTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final Function(String) onSubmitted;
+  final String label;
+
+  AdaptativeTextField(
+      {this.controller,
+      this.keyboardType = TextInputType.text,
+      this.onSubmitted,
+      this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: CupertinoTextField(
+                controller: controller,
+                keyboardType: keyboardType,
+                onChanged: onSubmitted,
+                placeholder: label,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 6,
+                  vertical: 12,
+                )),
+          )
+        : TextField(
+            keyboardType: keyboardType,
+            controller: controller,
+            onSubmitted: onSubmitted,
+            decoration: InputDecoration(
+              labelText: label,
+            ),
+          );
+  }
+}
